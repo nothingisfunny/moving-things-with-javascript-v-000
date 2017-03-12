ba79dcf06722ee067d0b5e2d3f73ffc45848cae9
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-03-12 12:33:27 -0700

    move left and right

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function init(){
+
+	var dodger = document.getElementById('dodger');
+
+	function moveDodgerLeft() {
+  	var leftNumbers = dodger.style.left.replace('px', '')
+  	var left = parseInt(leftNumbers, 10)
+ 
+ 	 if (left > 0) {
+    	dodger.style.left = `${left - 10}px`
+  		}
+	}
+
+	function moveDodgerRight() {
+  	var leftNumbers = dodger.style.left.replace('px', '')
+  	var left = parseInt(leftNumbers, 10)
+ 
+ 	 if (left < 360) {
+    	dodger.style.left = `${left + 10}px`
+  		}
+	}
+
+	document.addEventListener('keydown', function(e) {
+  		if (e.which === 37) {
+   			moveDodgerLeft()
+  		}else if(e.which === 39)	{
+  			console.log("Right arrow down!")
+  			moveDodgerRight()
+  		}
+	})
+}
